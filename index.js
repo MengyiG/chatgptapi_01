3820135230d48c6ba927eba930c8c2a54a997237
mengyi <yiii9292@gmail.com>
2023-04-02 22:00:32 -0400

    add openai api configuration

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // A express server, which will handle api requests coming in and respond back with a json object.
 // It will use body parser as well as cors
+const OpenAI = require("openai");
+const { Configuration, OpenAIApi } = OpenAI;
+
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -7,11 +10,31 @@ const cors = require("cors");
 const app = express();
 const port = 3001;
 
+const configuration = new Configuration({
+    organization: "org-1tHwOsImlAF3VVvFrbGUKbNP",
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/", (req, res) => {
-  res.send({ message: "Hello World!" });
+app.post("/", async(req, res) => {
+    const { message } = req.body;
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Hello, this is ChatGPT. How can I can you today? 
+        Person: I want some motivation.
+        ChatGPT: You are amazing! You can do anything you want.
+        ${message}`,
+        max_tokens: 500,
+        temperature: 0,
+    });
+    console.log(response.data)
+    if(response.data.choices[0].text){
+    res.json({ 
+        message: response.data.choices[0].text})
+    };
 });
 
 app.listen(port, () => {
--- src/App.js
+++ src/App.js
@@ -23,14 +23,30 @@ function App(){
 
   return (
     <div className="App">
+      <h1>对话 😁 ChatGPT</h1>
       <form onSubmit={handleSubmit}>
         <label>
           Message:
-          <textarea value={message} onChange={e => setMessage(e.target.value)} />
+          <textarea class = "textbox" value={message} 
+          placeholder="Hey, ask me something!"
+          onChange={e => setMessage(e.target.value)} />
         </label>
         <input type="submit" value="Submit" />
       </form>
       <p>{response}</p>
+      <br></br>
+      <h2> FUN facts.</h2>
+      <ol>
+        <ul>ChatGPT was launched on 30 November 2022.</ul>
+        <ul>It is a chatbot that uses GPT-3 to generate responses to user input.</ul>
+        <ul>It has a daily of 13 million users.</ul>
+      </ol>   
+      <br></br>
+      <h2> Alternatives / Competitors </h2>
+      <h4>Bard - AI</h4>
+      <h4>ChatSonic - Writing</h4>
+      <h4>CoPilot - Coding</h4>
+      <h4>NeevaAI - Search engine</h4>
     </div>
   );
 }
